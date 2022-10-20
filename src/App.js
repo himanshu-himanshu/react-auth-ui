85e284c49c95fc9be8532bc403debcfab20f3f03
Himanshu <himanshu16315@gmail.com>
2022-10-20 02:50:08 -0400

    Login page UI implemented using tailwind

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Homepage from "./pages/Homepage";
+import Profile from "./pages/Profile";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <>
-      <Homepage />
+      <Router>
+        <div className="font-Josefin">
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,44 @@
 import React from "react";
 
+import { CiLogin } from "react-icons/ci";
+
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="min-h-screen flex-center">
+      <div className="w-full max-w-xl flex-center flex-col space-y-4">
+        {/** Heading */}
+        <div className="p-2">
+          <h1 className="text-7xl text-gray-700 tracking-wide">Login</h1>
+        </div>
+
+        {/** Inputs Container */}
+        <div className="flex flex-col space-y-8 w-full px-16 py-12">
+          {/** Inputs */}
+          <input type="email" placeholder="Email" className="input" />
+          <input type="password" placeholder="Password" className="input" />
+
+          {/** Button Container */}
+          <div className="relative group px-4 py-5 text-2xl gray-border text-center bg-white hover:cursor-pointer">
+            <div className="absolute w-full h-full top-1.5 left-1.5 -z-20 group-hover:top-1 group-hover:left-1 gray-border duration-200" />
+            <button className="tracking-wider font-bold flex flex-row justify-center items-center w-full">
+              Login <CiLogin className="ml-2" />
+            </button>
+          </div>
+
+          {/** Signup Container */}
+          <div className="flex-center flex-row space-x-2 p-2 text-lg">
+            <span className="tracking-wide">Don't have an account yet?</span>
+            <a
+              href="/register"
+              className="font-[900] hover:-translate-y-0.5 duration-200"
+            >
+              Sign Up
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
