83d36b0c43044e9328607d7b29bbf89df377c1b2
Himanshu <himanshu16315@gmail.com>
2022-10-21 22:03:47 -0400

    Show and hide password implemented

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,26 +1,62 @@
 import React from "react";
+import { useState } from "react";
 
 import { CiLogin } from "react-icons/ci";
+import { FiEye, FiEyeOff } from "react-icons/fi";
+import { TbUserCheck } from "react-icons/tb";
 
 function Login() {
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleOnPasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
   return (
-    <div className="min-h-screen flex-center">
+    <div className="relative min-h-screen flex-center">
       <div className="w-full max-w-xl flex-center flex-col space-y-4">
         {/** Heading */}
         <div className="p-2">
-          <h1 className="text-7xl text-gray-700 tracking-wide">Login</h1>
+          <h1 className="heading">Login</h1>
         </div>
 
         {/** Inputs Container */}
         <div className="flex flex-col space-y-8 w-full px-16 py-12">
           {/** Inputs */}
-          <input type="email" placeholder="Email" className="input" />
-          <input type="password" placeholder="Password" className="input" />
+          <input type="email" placeholder="Email" className="input" required />
+          <div className="placeholder:tracking-wide flex flex-row justify-between py-5 px-2 text-xl border border-gray-400">
+            <input
+              type={showPassword ? "text" : "password"}
+              placeholder="Password"
+              className="focus:outline-none active:border-gray-600 focus:border-gray-600 w-4/5"
+              value={password}
+              onChange={(e) => handleOnPasswordChange(e)}
+              required
+            />
+            <div className="cursor-pointer w-1/5 flex justify-end items-center mr-2">
+              {!showPassword && (
+                <FiEye
+                  className="text-gray-400"
+                  onClick={() => handleShowPassword()}
+                />
+              )}
+              {showPassword && (
+                <FiEyeOff
+                  className="text-gray-400"
+                  onClick={() => handleShowPassword()}
+                />
+              )}
+            </div>
+          </div>
 
           {/** Button Container */}
-          <div className="relative group px-4 py-5 text-2xl gray-border text-center bg-white hover:cursor-pointer">
-            <div className="absolute w-full h-full top-1.5 left-1.5 -z-20 group-hover:top-1 group-hover:left-1 gray-border duration-200" />
-            <button className="tracking-wider font-bold flex flex-row justify-center items-center w-full">
+          <div className="primary-btn__container group">
+            <div className="primary-btn__overlay" />
+            <button className="primary-btn">
               Login <CiLogin className="ml-2" />
             </button>
           </div>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,80 @@
 import React from "react";
+import { useState } from "react";
+
+import { FiEye, FiEyeOff } from "react-icons/fi";
+import { TbUserCheck } from "react-icons/tb";
 
 function Register() {
-  return <div>Register</div>;
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleOnPasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <div className="relative min-h-screen flex-center">
+      <div className="w-full max-w-xl flex-center flex-col space-y-2">
+        {/** Heading */}
+        <div className="p-2">
+          <h1 className="heading">Signup</h1>
+        </div>
+
+        {/** Inputs Container */}
+        <div className="flex flex-col space-y-6 w-full px-16 py-12">
+          {/** Inputs */}
+          <input type="text" placeholder="Username" className="input" />
+          <input type="email" placeholder="Email" className="input" />
+          <div className="placeholder:tracking-wide flex flex-row justify-between py-5 px-2 text-xl border border-gray-400">
+            <input
+              type={showPassword ? "text" : "password"}
+              placeholder="Create Password"
+              className="focus:outline-none active:border-gray-600 focus:border-gray-600 w-4/5"
+              value={password}
+              onChange={(e) => handleOnPasswordChange(e)}
+              required
+            />
+            <div className="cursor-pointer w-1/5 flex justify-end items-center mr-2">
+              {!showPassword && (
+                <FiEye
+                  className="text-gray-400"
+                  onClick={() => handleShowPassword()}
+                />
+              )}
+              {showPassword && (
+                <FiEyeOff
+                  className="text-gray-400"
+                  onClick={() => handleShowPassword()}
+                />
+              )}
+            </div>
+          </div>
+
+          {/** Button Container */}
+          <div className="primary-btn__container group">
+            <div className="primary-btn__overlay" />
+            <button className="primary-btn">
+              Register <TbUserCheck className="ml-2" />
+            </button>
+          </div>
+          {/** Signup Container */}
+          <div className="flex-center flex-row space-x-2 p-2 text-lg">
+            <span className="tracking-wide">Already have an account?</span>
+            <a
+              href="/login"
+              className="font-[900] hover:-translate-y-0.5 duration-200"
+            >
+              Login
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Register;
