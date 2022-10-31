d257c97a59aa80fd079da6c66f1f086e75770263
Himanshu <himanshu16315@gmail.com>
2022-10-30 20:57:37 -0400

    Style changes for errors

--- src/Validations/Validation.js
+++ src/Validations/Validation.js
@@ -3,16 +3,19 @@ import * as yup from "yup";
 export const userSchema = yup.object().shape({
   username: yup
     .string()
-    .required("Username is required")
-    .min(6, "Username too short"),
+    .required("⚠ Username is required")
+    .min(6, "⚠ Create a username atleast 6 characters long.")
+    .matches(
+      "^(?=[a-zA-Z0-9._]{6,20}$)(?!.*[_.]{2})[^_.].*[^_.]$",
+      "⚠ Given username contains invalid characters"
+    ),
   email: yup
     .string()
-
-    .email("Please enter a valid email")
-    .required("Email is required"),
+    .email("⚠ Please enter a valid email address.")
+    .required("⚠ Email is required"),
   password: yup
     .string()
-    .min(6, "Password too short")
-    .max(12, "Password too long")
-    .required("Password is required"),
+    .min(8, "⚠ Create a password between 8-12 characters")
+    .max(12, "⚠ Create a password between 6-12 characters")
+    .required("⚠ Password is required"),
 });
